c2471a27a9946695bc5d84735be382dfc224f1d4
Cliff Jurgensen <jivefox.002@gmail.com>
2019-01-25 21:57:02 +0000

    Updated character and cli files

--- lib/VentureVerse/character.rb
+++ lib/VentureVerse/character.rb
@@ -2,10 +2,20 @@ class character
 
   attr_accessor :name, :episodes, :voice_actor
 
+  @@all = []
+
   def initialize(name, url)
     @name = name
     @url = url
-    @episodes = []
+    self << @@all
+  end
+
+  def self.all
+    @@all
+  end
+
+  def list_characters
+    self.all.each.with_index(1) {|character, i| puts "#{i}. #{character.name}"}
   end
 
 end
--- lib/VentureVerse/cli.rb
+++ lib/VentureVerse/cli.rb
@@ -1,7 +1,50 @@
+require 'pry'
+
 class VentureVerse::CLI
 
   def call
-    puts "Enter a Venture Bros. character with whom you'd like to better familiarize yourself."
+    main_menu
   end
+
+  def main_menu
+    puts "Would you like to better understand the involuted universe of the Venture Bros. via character, episode or voice-actor?"
+    input = gets.strip.downcase
+    case input
+    when "character"
+      list_characters
+    when "episode"
+      list_episodes
+    when "voice-actor"
+      list_voice_actors
+    end
+  end
+
+  def character_menu
+    puts "Enter the name of the character with whom you would like to better familiarize yourself or type 'list' to see a full list of characters.  Type 'main menu' to return to main menu.  Type 'exit' if you want to, you know, exit."
+    input = gets.strip.downcase
+    case input
+    when Character.all.include?(input)
+      character = input
+      character_details(character)
+      # Should return instace of character with hash of info scraped from web that matches input
+  end
+
+  def list_character_names
+    puts "Enter the number of a character to get more info."
+    list_characters
+    input = gets.strip.to_i
+    when (1..Character.all.length).include?(input)
+      character = Character.all[input - 1]
+      character_details(character)
+    else
+      invalid_character
+      list_characters
+  end
+
+  def invalid_character
+    puts "Who dat?"
+    list_characters
+  end
+
 
 end
