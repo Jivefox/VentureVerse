2c085688e893ec2bf8920dee0d1c81331684701f
Cliff Jurgensen <jivefox.002@gmail.com>
2019-01-26 02:51:27 +0000

    Updated scraper file

--- lib/VentureVerse/scraper.rb
+++ lib/VentureVerse/scraper.rb
@@ -1,3 +1,21 @@
+require 'pry'
+
 class Scraper
 
+  VENTURE_VERSE_URL = "https://venturebrothers.fandom.com"
+
+  def self.scrape_characters
+    html = open('VENTURE_VERSE_URL + character.url')
+    doc = Nokogiri::HTML(html)
+
+    doc.css()
+
+    char_urls = page.css("li.category-page__member a")
+    char_list = char_urls.map do  |element|
+    url = element unless element.text.include?('Category')
+    name = element.text unless element.text.include?('Category')
+  end
+    char_list.compact.each.with_index(1) do |char, i|
+    puts "#{i}. #{char}"
+  end
 end
