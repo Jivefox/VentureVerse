0d991cb074802e87cf4fbe412eef51577e17b710
Cliff Jurgensen <jivefox.002@gmail.com>
2019-01-28 23:06:25 +0000

    created custom constructor for Character class

--- lib/VentureVerse/character.rb
+++ lib/VentureVerse/character.rb
@@ -18,4 +18,19 @@ class Character
     self.all.each.with_index(1) {|character, i| puts "#{i}. #{character.name}"}
   end
 
+  def self.new_from_vb(url)
+    html = open("https://venturebrothers.fandom.com/Category:Characters")
+    page = Nokogiri::HTML(html)
+    char.name = page.css('h1.page-header__title').text
+    char.first_appearance =  page.css("")
+    char.voiced by = page.css("")
+    char.aliases = page.css("")
+    char.occupation = page.css("")
+    char.relatives = page.css("")
+    char.notable_characteristics = page.css("")
+    char.notable_alliances = page.css("")
+    char.enemies = page.css("")
+    @@all << self
+  end
+
 end
