336d639a757a49f88b1ae9fe70c18dd3607c68c9
Cliff Jurgensen <jivefox.002@gmail.com>
2019-01-31 15:40:52 +0000

    added while loop to cli

--- lib/VentureVerse/cli.rb
+++ lib/VentureVerse/cli.rb
@@ -6,7 +6,8 @@ class VentureVerse::CLI
   end
 
   def main_menu
-      puts "Would you like to explore the involuted universe of the Venture Bros. via character, episode or voice actor?"
+    puts "Would you like to explore the involuted universe of the Venture Bros. via character, episode or voice actor?"
+    while input !=  "exit"
       input = gets.strip.downcase
       case input
       when "character"
@@ -17,6 +18,7 @@ class VentureVerse::CLI
         list_voice_actors
       else
         puts "Spanikopita!!  Sorry. If you'd like a valid response, please enter a valid command."
+      end
     end
   end
 
@@ -25,31 +27,35 @@ class VentureVerse::CLI
     Scraper.scrape_character_directory_2
     Character.list_characters
     puts "Enter the number of the character with whom you would like to better familiarize yourself.  Type 'main menu' to return to main menu.  Type 'exit' if you want to, you know, exit."
-    input = gets.strip
+    while input != "exit"
+      input = gets.strip
 
-    case input
-    when (1..Character.all.length).include?(input.to_i)
-      character = Character.all[input.to_i - 1]
-      character
-    when "main menu"
-      main_menu
-    else
-      invalid_character
+      case input
+      when (1..Character.all.length).include?(input.to_i)
+        character = Character.all[input.to_i - 1]
+        character
+      when "main menu"
+        main_menu
+      else
+        invalid_character
+      end
     end
   end
 
   def episode_menu
     Scraper.list_episodes
     puts "Enter the number of the episode with which you'd like to better familiarize yourself.  Type 'main menu' to return to main menu.  Type 'exit' if you want to, you know, exit."
-    input = gets.strip
+    while input != "exit"
+      input = gets.strip
 
-    if (1..Episode.all.length).include?(input)
-      episode = Episode.all[input.to_i - 1]
-      Scraper.scrape_episode(episode)
-    elsif "main menu"
-      main_menu
-    else
-      invalid_episode
+      if (1..Episode.all.length).include?(input)
+        episode = Episode.all[input.to_i - 1]
+        Scraper.scrape_episode(episode)
+      elsif "main menu"
+        main_menu
+      else
+        invalid_episode
+      end
     end
   end
 
